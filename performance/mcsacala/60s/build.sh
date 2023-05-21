271ff6faf4182486551a11cad0e8ae15b46ac48b
UoFixED <42113547+UoFixED@users.noreply.github.com>
2023-05-21 21:00:05 +0200

    test

---
+++ performance/mcsacala/60s/build.sh
@@ -0,0 +1,3 @@
+docker build -t chuangtw/mcsacala60 .
+docker login
+docker push chuangtw/mcsacala60
---
+++ performance/mcsv2after/60s/build.sh
@@ -0,0 +1,3 @@
+docker build -t chuangtw/mcsv2after60 .
+docker login
+docker push chuangtw/mcsv2after60
